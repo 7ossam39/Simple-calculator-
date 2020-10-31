a598a904641f92ce0e121057f5f8d6d8ec242acb
hossam <55478484+7ossam39@users.noreply.github.com>
2020-10-31 19:27:25 +0300

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+num1 = int(input(' Enter your num1 :'))
+ope = input('Enter your ope :')
+num2 = int(input(' Enter your num2 :'))
+
+if ope == '+':
+    print(num1 + num2 )
+
+elif ope == '-':
+    print(num1 - num2 )
+
+elif ope == '*':
+    print(num1 * num2 )
+
+elif ope == '/':
+    print(num1 / num2 )
+
+input('Press enter to exit...')
+
